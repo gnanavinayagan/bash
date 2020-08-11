2851222dcd57eee4d4a5ceef231d1a81c00506c9
gnanavinayagan <gnanavinayagan@iitb.ac.in>
2020-08-11 18:21:39 +0530

    clone script

---
+++ gitclone.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+git clone  https://github.com/gnanavinayagan/mybash.git
+git clone  https://github.com/gnanavinayagan/myvim.git
+git clone  https://github.com/gnanavinayagan/myrhel.git
+
+
+cd mybash && git remote add mybash https://github.com/gnanavinayagan/mybash.git && cd ..
+cd myvim && git remote add myvim https://github.com/gnanavinayagan/myvim.git && cd ..
+cd myrhel && git remote add myrhel https://github.com/gnanavinayagan/myrhel.git && cd ..
+
+
